08de59407c908112d7a971ffbe3a7c848f5450f1
jennclee <8753352+jennclee@users.noreply.github.com>
2017-09-21 18:49:26 -0700

    Moved user history element

--- twittler.js
+++ twittler.js
@@ -3,42 +3,44 @@ $(document).ready(function() {
 
     var index = streams.home.length - 1;
     while (index >= 0) {
-        var $tweetBody = $('<div class="tweet"></div>');
-        var $userHist = $('<div class="userHist"></div>')
-        var tweet = streams.home[index];
-        var user = tweet.user;
-        var message = tweet.message;
-        var date = tweet.created_at;
-        var userHist = streams.users[user];
+      var $tweetBody = $('<div class="tweet"></div>');
+      var $userHist = $('<div class="userHist"></div>');
+      var tweet = streams.home[index];
+      var user = tweet.user;
+      var message = tweet.message;
+      var date = tweet.created_at;
+      var userHist = streams.users[user];
 
-        // Populate user history
-        _.each(userHist, function(histItem) {
-            var itemDate = histItem.created_at;
-            var itemMsg = histItem.message;
-            var $itemHist = $('<p class="hist"></p>');
-            $itemHist.text(itemMsg + itemDate);
-            $itemHist.appendTo($userHist);
-        });
+      // Populate user history
+      _.each(userHist, function(histItem) {
+          var itemDate = histItem.created_at;
+          var itemMsg = histItem.message;
+          var $itemHist = $('<p class="hist"></p>');
+          $itemHist.text(itemMsg + itemDate);
+          $itemHist.appendTo($userHist);
+      });
 
-        // Show user names
-        var $user = $('<a href="#" class="user"></a>');
-        $user.text(user);
-        $user.appendTo($tweetBody);
-        $userHist.appendTo($tweetBody);
-        $userHist.hide();
+      // Show user names
+      var $user = $('<a href="#" class="user"></a>');
+      $user.text('@' + user);
+      $user.appendTo($tweetBody);
 
-        // Show messages
-        var $msg = $('<p class="message"></p>');
-        $msg.text(message);
-        $msg.appendTo($tweetBody);
+      // Show messages
+      var $msg = $('<p class="message"></p>');
+      $msg.text(message);
+      $msg.appendTo($tweetBody);
 
-        // Show timestamp
-        var $date = $('<p class="date"></p>');
-        $date.text(date);
-        $date.appendTo($tweetBody);
+      // Show timestamp
+      var $date = $('<p class="date"></p>');
+      $date.text(date);
+      $date.appendTo($tweetBody);
 
-        $tweetBody.appendTo($body);
-        index -= 1;
+      $userHist.appendTo($tweetBody);
+      $userHist.hide();
+
+      $tweetBody.appendTo($body);
+
+      index -= 1;
     }
 
     // Show user history onclick
